8e6f0574103b7f57b457c79fea04f6c2bf96b6bb
MalikAwan <malik.s.awan@live.com>
2018-12-06 11:54:49 -0600

    working but dont have dates yet

---
+++ PyBank/main.py
@@ -0,0 +1,56 @@
+import os
+import csv
+print(" Financial Analysis ")
+print("--------------------------------------")
+budgetCSV = os.path.join("." , "Resources", "Budget_Data.csv")
+with open(budgetCSV, newline='') as csvfile:
+    count = 0
+    Total = 0
+    
+    all_values = []
+    profit_change = []
+    greatest_increase = 0
+    greatest_decrease = 0
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+    firstItem = 0
+    secondItem = 0
+    
+    for row in csvreader:
+        all_values.append(row[1])
+        count = count + 1
+        Total = Total + int(row[1])  
+        
+    
+    for index in range(len(all_values)-1):
+        
+        firstItem = int(all_values[index])
+        secondItem = int(all_values[index+1])
+        difference = secondItem - firstItem
+        profit_change.append(difference)
+        #maxm = max(profit_change)
+        #minm  = min(profit_change)
+        if difference > greatest_increase:
+            greatest_increase = difference
+        else:
+            greatest_increase = greatest_increase
+        #date1 = index[0]
+        if difference < greatest_decrease:
+            greatest_decrease = difference
+        else:
+            greatest_decrease = greatest_decrease    
+            #Date1 = index[0]
+    #for i in range(len(profit_change)):
+        #if int(profit_change[i]) == maxm:
+            #Date1 = int(i[0])
+        #if int(profit_change[i]) == minm:
+            #Date2 = i[0]
+    average_Change = sum(profit_change)/(count-1)
+    print(f"Total number of months : {count}")
+    print(f"Total : {Total}")
+    print(f"Average change : {round(average_Change,2)}")
+    print(f"Greatest increase in profit : {greatest_increase}")
+    print(f"Greatest decrease in profit : {greatest_decrease}")
+    #getprint(row[0])
+
+
