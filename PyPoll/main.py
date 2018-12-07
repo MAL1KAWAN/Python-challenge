16b1bd29bc6d66f037cf9896f1d3a98179a1fa4d
MalikAwan <malik.s.awan@live.com>
2018-12-06 20:56:34 -0600

    Complete code

---
+++ PyPoll/main.py
@@ -0,0 +1,31 @@
+import os
+import csv
+print("Election Results ")
+print("------------------------------")
+budgetCSV = os.path.join("." , "Resources", "election_data.csv")
+with open(budgetCSV, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+    count = 0
+    votes = {}
+    percentage = 0
+    winner = 0
+    for row in csvreader:
+        count += 1
+        candidates = row[2]
+        if candidates in votes:
+            votes[candidates] = votes[candidates]+1
+        else:
+            votes[candidates] = 1
+    print(f"Total votes : {count}") 
+    print("------------------------------")
+
+    maxVote = max(votes.values())
+    for x, y in votes.items():
+        percentage = (y/count)
+        if y == maxVote:
+            winner = x 
+        print(x,(" "+"{:.2%}".format(percentage)),"(", y, ")")
+    print("------------------------------")
+    print(f"Winner : {winner}")
+    print("------------------------------")
